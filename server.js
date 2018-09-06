020fc2cffecaf617b9aee7415d7de62186a68f36
Dan Hayes <hayesdc01@gmail.com>
2018-09-06 14:58:26 +0100

    updated route response

--- server.js
+++ server.js
@@ -22,9 +22,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// app.get('/', (req, res) => {
-// 	res.json(database.users);
-// })
+app.get('/', (req, res) => { res.json('app is working fine') })
 
 app.post('/signin', (req, res) => { signin.handleSignIn(req, res, knex, bcrypt) })
 
